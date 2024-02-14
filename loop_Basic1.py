9195f58e9d5713ccef2be93a4ce8ff8f36567380
Balqees Awawdeh <balqisawawde@gmail.com>
2024-02-14 22:30:02 +0200

    first commit

---
+++ loop_Basic1.py
@@ -0,0 +1,30 @@
+for i in range(1,151):
+    print(i)
+
+for x in range (5,1000):
+    if x%5==0:
+        print (x)
+
+for y in range(1,100):
+    if y%10==0:
+        print ("Coding")
+    elif y%5==0: 
+        print("Coding Dojo")
+    else:
+        print (y)
+sum=0
+for s in range(0,500000):
+    if s % 2 != 0:
+        sum +=s
+print(sum)
+
+
+for z in range(2018,0,-4):
+    print(z)
+
+lowNum=2
+highNum=10
+multi=3
+for o in range(lowNum,highNum):
+    if o%multi==0:
+        print(o)
