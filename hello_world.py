8f3f23f8f0c955980b624b7f8a65ef411881997f
Balqees Awawdeh <balqisawawde@gmail.com>
2024-02-16 09:28:45 +0200

    New file

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,23 @@
 
 print("Hello World")
-x = "Hello Python"
-print(x)
-y = 42
-print(y)
+x = "Balqees"
+print("Hello",x)
+print("Hello"+x)
+num=100
+print("Hello",num)
+print("Hello"+str(num))
+
+a="Makloopa"
+b="Mansaf"
+print("i love to eat "+ a +" and "+ b)
+print(f"i love to eat {a} and {b}")
+
+Fname = "Balqees"
+Lname = "Awawdeh" 
+message = "My name is {} and I am {} years old."
+formatted_message = message.format(Fname, Lname)
+print(formatted_message)
+
+
 
 
